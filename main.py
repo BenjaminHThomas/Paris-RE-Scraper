eda602e13525d13cc6f291971778c213dcc4850c
Ben <benjamin.h.thomas1@gmail.com>
2023-12-20 17:48:14 +0100

    Added warning and instructions to main

--- main.py
+++ main.py
@@ -4,18 +4,28 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 """
-The 'main' script is mainly for convenience and showing the intended syntax.
-You could just as easily run the below commands in the terminal.
-
 Currently there's only one scraper, which is for BienIci.com.
 The only 2 valid inputs for the scrape function are 'buy' and 'rent'. 
 
-It isn't mandatory to use both as I've done below,
+It isn't mandatory to use both 'buy' and 'rent' as I've done below,
 but the intention is for the rent data to supplement the buy data.
 The data's intended purpose is to answer questions such as:
     - "If I buy an apartment in neighbourhood x, with y characteristics, what rental yield (ROI) can I reasonably expect?"
     - "I currently own an apartment with x characterisitcs in neighbourhood y, what could I reasonably sell or rent it for?"
     - "How are rent / property prices changing over time in each neighbourhood?"
+
+before running the script, please ensure your .env file is set up with your mysql details. For example:
+DB_HOST=localhost
+DB_USER=username
+DB_PASSWORD=password
+Also please tweak the settings in the settings.py file.
+
+If the script fails to scrape it's likely one of two things:
+- The website has changed and class names are different
+- Your IP has been flagged as a bot and you're now banned.
+
+If you can access the website manually, it's likely the first reason. 
+You can validate this by inspecting the webpage and searching for the missing element.
 """
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 
 # This variable determines the number of home pages that are scraped for properties. The home page contains all the individual properties.
 ## There's 100 pages in BienIci
-property_page_limit = 2
+property_page_limit = 100
 
 ## Number of times the script tries to get a new 
 max_retry = 5
